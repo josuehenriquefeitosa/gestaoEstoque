3844ad3cdd7db3489debc647086dc384518e336d
Josué Feitosa <92409871+josuehenriquefeitosa@users.noreply.github.com>
2023-05-21 16:44:09 -0300

    atualizando index.js

--- assets/index.js
+++ assets/index.js
@@ -8,50 +8,60 @@
     console.log('INSERT INTO CLIENTES');
     const resultado = await data_base.insertCustomer({nome:`fernando`,telefone:`798888888888`,email:`fernando@email.com`,cpf:`05653524581`,data_nascimento:`1974-05-12`,sexo:`Masculino`,logradouro:`Av augusto franco`,numero:`2787`,complemento:`casa`,estado:`SE`,cidade:`Aracaju`
     });
-    console.log(resultado);
+    console.log(resultado[0]);
 */
 /*
     console.log('-----------------------------')
     console.log('SELECT * FROM CLIENTES');
     const clientes = await data_base.selectCustomers();
-    console.log(clientes);
+    console.log(clientes[0]);
     console.log('-----------------------------')
 */
-    console.log('')
+    console.log()
+    console.log()
     console.log('-----------------------------')
     console.log('SELECT * FROM CLIENTES WHERE nome= ?');
     const clientesNome = await data_base.selectCustomerName({nome:'josue'});
     console.log(clientesNome[0]);
     console.log('-----------------------------')
+    console.log()
 /*
+    console.log()
     console.log('-----------------------------')
     console.log('UPDATE CLIENTES');
     const resultado2 = await data_base.updateCustomer(3,{nome:``,telefone:``,cpf:``,data_nascimento:``,sexo:``,logradouro:``,numero:``,complemento:``,estado:``,cidade:``,email:``
     });
-    console.log(resultado2);
+    console.log(resultado2[0]);
     console.log('-----------------------------')
-
-
+    console.log()
+    console.log()
     console.log('-----------------------------')
     console.log('DELETE FROM CLIENTES');
     const resultado3 = await data_base.deleteCustomer();
-    console.log(resultado3);
+    console.log(resultado3[0]);
     console.log('-----------------------------')
+    console.log()
         // ----------------------------CLIENTES---------------------------------------
     */
 
         // ----------------------------PRODUTOS---------------------------------------
+        /*
+    console.log()
     console.log('-----------CADASTRO PRODUTOS------------------')
     console.log('INSERT INTO PRODUTOS');
     const resultado4 = await data_base.insertProduct({nome:`valvulaEscape`,preco:`10`,quantidade:`2`});
     console.log(resultado4[0]);
-
+    console.log()
+    console.log()
+    */
 
     console.log('------------MOSTRAR PRODUTOS-----------------')
     console.log('SELECT * FROM PRODUTOS');
     const produtos = await data_base.selectProduct();
     console.log(produtos[0]);
     console.log('-----------------------------')
+    console.log()
+    console.log()
 
 
     console.log('')
@@ -60,6 +70,8 @@
     const produtosNome = await data_base.selectProductName({nome:'oleo10w40'});
     console.log(produtosNome[0]);
     console.log('-----------------------------')
+    console.log()
+    console.log()
 
 
     console.log('-----------DELETAR PRODUTOS------------------')
